71275cc9701fba36cc45d7ae7c70cc1d458c1c9d
Dibendu Ghosal <128916258+dibendughosal@users.noreply.github.com>
2025-02-07 01:11:14 +0530

    JioCinema - React + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from '../src/pages/Home';
+function App() {
+
+  return (
+    <>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/home" element={< Home />}></Route>
+          </Routes>
+        </BrowserRouter>
+    </>
+  )
+}
+
+export default App
