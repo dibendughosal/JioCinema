fec166e24bb731e6c1214ca2b17364039695ba90
Dibendu Ghosal <128916258+dibendughosal@users.noreply.github.com>
2025-02-08 00:31:43 +0530

    JioCinema - React + Tailwind

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
-import Header from './Header';
+import Header from '../component/Header';
+import Tags from '../component/Tags';
+import Channels from '../component/Channels';
 function Home() {
   return (
     <div>
       <Header/>
+      <Tags/>
+      <Channels/>
     </div>
   )
 }
